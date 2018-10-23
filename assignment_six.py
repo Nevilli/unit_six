a9eb0f4288ac062a9bb59fd06ce896e335c5a00f
Lliam <lliam@iMac8.local>
2018-10-23 10:16:27 -0400

    Created birthday check function

--- assignment_six.py
+++ assignment_six.py
@@ -1,9 +1,22 @@
 import random
 
+
 def birthday_list():
     birthdays = []
     for x in range(23):
-        birthdays.append(random.randint(1, 366))
+        birthdays.append(random.randint(1, 365))
     print(birthdays)
+    return birthdays
 
-birthday_list()
+
+def list_check(birthdays):
+    check_birthdays = set(birthdays)
+    if len(check_birthdays) < len(birthdays)        
+
+
+
+
+
+
+dates = birthday_list()
+list_check(dates)
