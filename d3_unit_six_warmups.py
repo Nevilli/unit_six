91e831a2f2899c92eb388bbfb2263849a9ee9824
Lliam <lliam@iMac8.local>
2018-10-23 09:18:33 -0400

    Modifying a list

---
+++ d3_unit_six_warmups.py
@@ -0,0 +1,9 @@
+students = ["Harry", "Ron", "Hermione", "Ginny", "Fred", "Neville", "Draco"]
+students.remove("Fred")
+print(students)
+del students[4]
+print(students)
+students[1] = "Bill"
+print(students)
+students.append("Parvati")
+print(students)
