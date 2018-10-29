400ac34f10915b32becab3876439f2f9fead67c3
Lliam <lliam@iMac8.local>
2018-10-29 14:53:16 -0400

    Finished project

--- assignment_six.py
+++ assignment_six.py
@@ -1,7 +1,17 @@
+# Liam Neville
+# 9/29/18
+# This program creates a random list of birthdays and checks for matches, if there are matches, they are calculated
+# into a percent.
+
+
 import random
 
 
 def birthday_list():
+    """
+    This function creates the random list of birthdays
+    :return: birthdays is a list of 23 values representing birthdays and is returned to be checked for matches
+    """
     birthdays = []
     for x in range(23):
         birthdays.append(random.randint(1, 365))
@@ -9,6 +19,13 @@ def birthday_list():
 
 
 def list_check(birthdays):
+    """
+    This function checks the list of birthdays for matches
+    :param birthdays: This is the random list of birthdays
+    :return: True is returned only if there is a match
+    False is returned if there is no matches
+    """
+# set is used below because there is a collection of values
     check_birthdays = set(birthdays)
     if len(check_birthdays) < len(birthdays):
         return True
@@ -23,7 +40,9 @@ def main():
         dates = birthday_list()
         if list_check(dates) is True:
             matches = matches + 1
-    print("There were", matches, "matches")
+    print("There were", matches, "matches out of", how_many)
+    percent = matches / how_many * 100
+    print("That is", percent, "percent")
 
 
 main()
